0f0d690e3ffe908a8a6a8c1a515f4c7c031ecb18
Thomas Nishiduka <thomas.nishiduka@gmail.com>
2023-06-30 14:27:33 -0300

    feat: first commit

---
+++ src/App.tsx
@@ -0,0 +1,91 @@
+import { useState } from 'react';
+import { Button, Container, Card } from 'react-bootstrap';
+import moment from 'moment';
+
+import './App.css';
+import DateInput from './components/DateInput';
+
+function App() {
+  const [quantityHours, setQuantityHours] = useState<string[]>(['00:00']);
+  const [quantityHoursExtra, setQuantityHoursExtra] = useState<string[]>([
+    '00:00',
+  ]);
+
+  const setTotalHoursinDay = (index: number, hours: string) => {
+    setQuantityHours((value) => {
+      value[index] = hours;
+
+      return value;
+    });
+  };
+
+  const setTotalHoursExtrainDay = (index: number, hours: string) => {
+    setQuantityHoursExtra((value) => {
+      value[index] = hours;
+
+      return value;
+    });
+  };
+
+  const addRowSum = () => {
+    setQuantityHoursExtra((value) => [...value, '00:00']);
+    setQuantityHours((value) => [...value, '00:00']);
+  };
+
+  const calculate = (rows: string[]) => {
+    return rows.reduce((acc, current) => {
+      const [hour, minute] = current.split(':');
+      const total = moment(acc, 'HH:mm');
+      total.add(hour, 'hours');
+      total.add(minute, 'minutes');
+
+      return `${total.hours().toString().padStart(2, '0')}:${total
+        .minutes()
+        .toString()
+        .padStart(2, '0')}`;
+    }, '00:00');
+  };
+
+  return (
+    <div className="content">
+      <Container>
+        <Card>
+          <Card.Body>
+            <Card.Title>Calculadora de Horas</Card.Title>
+            <hr />
+
+            <div className="grid">
+              <p>Entrada</p>
+              <p>Inicio almoco</p>
+              <p>Volta do almoco</p>
+              <p>Saida</p>
+              <p className="total-hours">Total</p>
+              <p className="total-hours-extra">Horas Extras</p>
+            </div>
+
+            {quantityHours.map((_, index) => (
+              <DateInput
+                key={index}
+                setTotalHoursExtra={(value) =>
+                  setTotalHoursExtrainDay(index, value)
+                }
+                setTotal={(value) => setTotalHoursinDay(index, value)}
+              />
+            ))}
+
+            <hr />
+            <div className="d-flex justify-content-center mt-4">
+              <Button onClick={addRowSum}>+</Button>
+            </div>
+
+            <hr />
+            <p>Total: {calculate(quantityHours)}</p>
+            <p>Total Horas extras: {calculate(quantityHoursExtra)}</p>
+          </Card.Body>
+        </Card>
+      </Container>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/DateInput/index.tsx
@@ -0,0 +1,135 @@
+import { useState, useEffect, useCallback } from 'react';
+import InputMask from 'react-input-mask';
+import moment from 'moment';
+
+import './styles.css';
+
+type TDateInput = {
+  setTotal: (value: string) => void;
+  setTotalHoursExtra: (value: string) => void;
+};
+const DateInput = ({
+  setTotal: setAllHours,
+  setTotalHoursExtra,
+}: TDateInput) => {
+  const [initHour, setInitHour] = useState('');
+  const [initLunchHour, setInitLunchHour] = useState('');
+  const [endLunchHour, setEndLunchHour] = useState('');
+  const [endHour, setEndHour] = useState('');
+
+  const [totalExtra, setTotalExtra] = useState('00:00');
+  const [total, setTotal] = useState('00:00');
+
+  const calculateDiffBetween = useCallback(
+    (end: string, start: string): string => {
+      const dt1 = moment(end, 'HH:mm');
+      const dt2 = moment(start, 'HH:mm');
+
+      if (!dt1.isValid() || !dt2.isValid()) return '';
+
+      const hours = dt2.diff(dt1, 'hours');
+      const minutes = dt2.diff(dt1, 'minutes') % 60;
+
+      if (hours < 0 || minutes < 0) {
+        return '00:00';
+      }
+
+      return `${hours.toString().padStart(2, '0')}:${minutes
+        .toString()
+        .padStart(2, '0')}`;
+    },
+    []
+  );
+
+  const calculateHours = useCallback(() => {
+    if (!initHour || !endHour) return;
+
+    const lunchTime = calculateDiffBetween(initLunchHour, endLunchHour);
+    const workTime = calculateDiffBetween(initHour, endHour);
+
+    let totalHours = calculateDiffBetween(
+      lunchTime.toString(),
+      workTime.toString()
+    );
+
+    if (!lunchTime) {
+      totalHours = workTime;
+    }
+
+    if (!totalHours) {
+      setTotal('');
+    }
+
+    setAllHours(totalHours.toString());
+    setTotal(totalHours.toString());
+
+    const extra = calculateDiffBetween('08:00', totalHours);
+
+    if (extra) {
+      setTotalHoursExtra(extra);
+      setTotalExtra(extra);
+    }
+  }, [
+    initHour,
+    endHour,
+    calculateDiffBetween,
+    initLunchHour,
+    endLunchHour,
+    setAllHours,
+    setTotalHoursExtra,
+  ]);
+
+  useEffect(() => {
+    calculateHours();
+  }, [calculateHours]);
+
+  const formatChars = {
+    2: '[0-2]',
+    5: '[0-5]',
+    9: '[0-9]',
+  };
+
+  const mask = '29:59';
+
+  return (
+    <div className="grid">
+      <InputMask
+        type="tel"
+        formatChars={formatChars}
+        mask={mask}
+        className="form-control"
+        value={initHour}
+        onChange={(e) => setInitHour(e.target.value)}
+      />
+      <InputMask
+        type="tel"
+        formatChars={formatChars}
+        mask={mask}
+        className="form-control"
+        value={initLunchHour}
+        onChange={(e) => setInitLunchHour(e.target.value)}
+      />
+      <InputMask
+        type="tel"
+        formatChars={formatChars}
+        mask={mask}
+        className="form-control"
+        value={endLunchHour}
+        onChange={(e) => setEndLunchHour(e.target.value)}
+      />
+      <InputMask
+        type="tel"
+        formatChars={formatChars}
+        mask={mask}
+        className="form-control"
+        value={endHour}
+        onChange={(e) => setEndHour(e.target.value)}
+      />
+
+      <p className="total-hours">{total}</p>
+      <p className="total-hours-extra">{totalExtra}</p>
+    </div>
+  );
+};
+
+export default DateInput;
